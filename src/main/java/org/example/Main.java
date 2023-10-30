abc3396625765fd175a91cdcf7d3cd20b0b7ae57
rishi2o2o <rishi.rvv@gmail.com>
2023-10-30 23:35:12 +0530

    added demo menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! I am rishi");
+    
+    	Scanner scn = new Scanner(System.in);
+    
+    	// Show the menu
+        System.out.println("MENU:");
+        System.out.println("1. Option1");
+        System.out.println("2. Option2");
+        System.out.println("3. Option3");
+        System.out.println("4. Option4");
+        System.out.println("5. Exit");
+        
+        
+        // Toke user's input
+        System.out.printf("\nEnter your choice: ");
+        int choice = scn.nextInt();
+        
+        
+        // Show user his input
+        System.out.println("> Your choice is: " + choice);
     }
 }
