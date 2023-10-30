dc76edcb64cc3c05a07f1b045d5f7dcf9ed535c4
rishi2o2o <rishi.rvv@gmail.com>
2023-10-31 03:31:27 +0530

    added logging

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,60 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Scanner;
 
 public class Main {
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
+
+    public static double addition(double a, double b) {
+
+        logger.info("[ADDITION OPERATION] [START] " + a + " " + b);
+
+        double res = a+b;
+
+        logger.info("[ADDITION OPERATION] [END] " + res);
+
+        return res;
+    }
+
+    public static double subtraction(double a, double b) {
+
+        logger.info("[SUBTRACTION OPERATION] [START] " + a + " " + b);
+
+        double res = a-b;
+
+        logger.info("[SUBTRACTION OPERATION] [END] " + res);
+
+        return res;
+    }
+
+    public static double multiplication(double a, double b) {
+
+        logger.info("[MULTIPLICATION OPERATION] [START] " + a + " " + b);
+
+        double res = a*b;
+
+        logger.info("[MULTIPLICATION OPERATION] [END] " + res);
+
+        return res;
+    }
+
+    public static double division(double a, double b) {
+
+        logger.info("[DIVISION OPERATION] [START] " + a + " " + b);
+
+        double res = a/b;
+
+        logger.info("[DIVISION OPERATION] [END] " + res);
+
+        return res;
+    }
+
+
     public static void main(String[] args) {
     
     	Scanner scanner = new Scanner(System.in);
@@ -28,28 +80,28 @@ public class Main {
                     a = scanner.nextDouble();
                     System.out.printf("Enter b: ");
                     b = scanner.nextDouble();
-                    System.out.printf("Result: %.2f + %.2f is %.2f \n", a, b, a+b);
+                    System.out.printf("Result: %.2f + %.2f is %.2f \n", a, b, addition(a,b));
                     break;
                 case 2:
                     System.out.printf("Enter a: ");
                     a = scanner.nextDouble();
                     System.out.printf("Enter b: ");
                     b = scanner.nextDouble();
-                    System.out.printf("Result: %.2f - %.2f is %.2f \n", a, b, a-b);
+                    System.out.printf("Result: %.2f - %.2f is %.2f \n", a, b, subtraction(a,b));
                     break;
                 case 3:
                     System.out.printf("Enter a: ");
                     a = scanner.nextDouble();
                     System.out.printf("Enter b: ");
                     b = scanner.nextDouble();
-                    System.out.printf("Result: %.2f * %.2f is %.2f \n", a, b, a*b);
+                    System.out.printf("Result: %.2f * %.2f is %.2f \n", a, b, multiplication(a,b));
                     break;
                 case 4:
                     System.out.printf("Enter a: ");
                     a = scanner.nextDouble();
                     System.out.printf("Enter b: ");
                     b = scanner.nextDouble();
-                    System.out.printf("Result: %.2f / %.2f is %.2f \n", a, b, a/b);
+                    System.out.printf("Result: %.2f / %.2f is %.2f \n", a, b, division(a,b));
                     break;
                 case 5:
                     System.out.println("Exiting...");
@@ -60,7 +112,6 @@ public class Main {
             }
             System.out.println();
 
-        } while(choice != 5);
-        
+        } while(choice != 5);       
     }
 }
