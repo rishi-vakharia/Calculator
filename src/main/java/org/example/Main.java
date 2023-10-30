55e37830d13ce715657541309f4fc00a8d27086b
rishi2o2o <rishi.rvv@gmail.com>
2023-10-31 01:32:16 +0530

    added calculator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,4 @@
-// package org.example;
+package org.example;
 
 import java.util.Scanner;
 
@@ -6,55 +6,61 @@ public class Main {
     public static void main(String[] args) {
     
     	Scanner scanner = new Scanner(System.in);
-        int choice, a, b;
+        int choice;
+        double a, b;
 
-        System.out.println("MENU:");
-        System.out.println("1. Addition");
-        System.out.println("2. Subtraction");
-        System.out.println("3. Multiplication");
-        System.out.println("4. Division");
-        System.out.println("5. Exit");
-        System.out.println();
-        System.out.printf("Enter choice: ");
-        choice = scanner.nextInt();
-        System.out.println();
+        do {
 
-        switch(choice) {
-            case 1:
-                System.out.println("For addition a+b");
-                System.out.printf("Enter a: ");
-                a = scanner.nextInt();
-                System.out.printf("Enter b: ");
-                b = scanner.nextInt();
-                System.out.printf("Result: %d+%d = %d \n", a, b, a+b);
-                break;
-            case 2:
-                System.out.println("For subtraction a-b");
-                System.out.printf("Enter a: ");
-                a = scanner.nextInt();
-                System.out.printf("Enter b: ");
-                b = scanner.nextInt();
-                System.out.printf("Result: %d-%d = %d \n", a, b, a-b);
-                break;
-            case 3:
-                System.out.println("For multiplication a*b");
-                System.out.printf("Enter a: ");
-                a = scanner.nextInt();
-                System.out.printf("Enter b: ");
-                b = scanner.nextInt();
-                System.out.printf("Result: %d*%d = %d \n", a, b, a*b);
-                break;
-            case 4:
-                System.out.println("For division a/b");
-                System.out.printf("Enter a: ");
-                a = scanner.nextInt();
-                System.out.printf("Enter b: ");
-                b = scanner.nextInt();
-                System.out.printf("Result: %d/%d = %d \n", a, b, a/b);
-                break;
-            case 5:
-                System.out.println("Exiting...");
-                break;
-        }
+            System.out.println("MENU:");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. Exit");
+            System.out.println();
+
+            System.out.printf("Enter choice: ");
+            choice = scanner.nextInt();
+
+            switch(choice) {
+                case 1:
+                    System.out.printf("Enter a: ");
+                    a = scanner.nextDouble();
+                    System.out.printf("Enter b: ");
+                    b = scanner.nextDouble();
+                    System.out.printf("Result: %.2f + %.2f is %.2f \n", a, b, a+b);
+                    break;
+                case 2:
+                    System.out.printf("Enter a: ");
+                    a = scanner.nextDouble();
+                    System.out.printf("Enter b: ");
+                    b = scanner.nextDouble();
+                    System.out.printf("Result: %.2f - %.2f is %.2f \n", a, b, a-b);
+                    break;
+                case 3:
+                    System.out.printf("Enter a: ");
+                    a = scanner.nextDouble();
+                    System.out.printf("Enter b: ");
+                    b = scanner.nextDouble();
+                    System.out.printf("Result: %.2f * %.2f is %.2f \n", a, b, a*b);
+                    break;
+                case 4:
+                    System.out.printf("Enter a: ");
+                    a = scanner.nextDouble();
+                    System.out.printf("Enter b: ");
+                    b = scanner.nextDouble();
+                    System.out.printf("Result: %.2f / %.2f is %.2f \n", a, b, a/b);
+                    break;
+                case 5:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice...");
+                    break;
+            }
+            System.out.println();
+
+        } while(choice != 5);
+        
     }
 }
